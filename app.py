a7c70e2f9fdc9fc52fab585da8ff1649439bd9ce
devmurilopires <murilopiresoficial@gmail.com>
2022-10-11 17:37:38 -0300

    Adicionando um print

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 print("rodando o sistema de gerenciamentos de jogos")
 print("Aplicação está se conectando ao banco de dados")
+ // mais um print de teste :
+print("O paulo fez esse cmmit sem você ver na aula!") 
+print("Enviando para o git hub")
